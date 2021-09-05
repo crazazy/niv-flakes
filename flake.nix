0c44a9824255858f4d7dd8bc93c60e97a53a2881
Erik Oosting <erik99.oosting@gmail.com>
2021-09-05 18:24:32 +0200

    update packages

--- flake.nix
+++ flake.nix
@@ -3,19 +3,19 @@ description = "A way to transport niv inputs to flake inputs";
 inputs = {
   flake-utils = {
     flake = true;
-    url = "https://github.com/numtide/flake-utils/archive/5466c5bbece17adaab2d82fae80b46e807611bf3.tar.gz";
+    url = "https://github.com/numtide/flake-utils/archive/997f7efcb746a9c140ce1f13c72263189225f482.tar.gz";
   };
   niv = {
     flake = false;
-    url = "https://github.com/nmattia/niv/archive/af958e8057f345ee1aca714c1247ef3ba1c15f5e.tar.gz";
+    url = "https://github.com/nmattia/niv/archive/e0ca65c81a2d7a4d82a189f1e23a48d59ad42070.tar.gz";
   };
   nixos-config = {
     flake = false;
-    url = "https://github.com/crazazy/nixos-config/archive/14060609e31f26969eae856d2693f17affa246f4.tar.gz";
+    url = "https://github.com/crazazy/nixos-config/archive/8e318c8773d0f8bed690a96ff9b74ce5f9d64d07.tar.gz";
   };
   nixpkgs = {
     flake = true;
-    url = "https://github.com/NixOS/nixpkgs/archive/1eea37190739485a01f5036558cd2ff257985678.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs/archive/eb73405ecceb1dc505b7cbbd234f8f94165e2696.tar.gz";
   };
 };
 outputs = args: (import ./dirtyFlake.nix).outputs args;
